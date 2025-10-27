5c534288fd18b7232f563b97e3cd6f29d3b9b339
Renata Meyer Hobold <renata.hobold@usp.br>
2025-10-27 18:31:55 -0300

    fix: corrigido tratamento de erro

--- solucoes.py
+++ solucoes.py
@@ -3,7 +3,30 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    deslocamento = deslocamento%26
+	# --------- tratamento de tipo errado no deslocamento
+    if isinstance(deslocamento, str):
+    	# se for uma string, tenta ver se é possível convertê-la num float
+        try:
+            deslocamento_float = float(deslocamento)
+        except ValueError:
+            raise TypeError(f"Deslocamento inválido: {deslocamento}")
+            
+    elif isinstance(deslocamento, (int, float)):
+    	# se recebeu número, converte pra float
+        deslocamento_float = float(deslocamento)
+        
+    else:
+        raise TypeError(f"Deslocamento deve ser int, float ou str numérica, recebeu {type(deslocamento).__name__}")
+    
+    if not deslocamento_float.is_integer():
+    	# dado o float, tenta converter pra int
+        raise TypeError(f"Deslocamento deve ser um inteiro, recebeu {deslocamento_float}")
+        
+    # --------- tratamento de tipo errado no texto
+    texto = str(texto)
+
+	# --------- começo da lógica da cifra
+    deslocamento = int(deslocamento_float)%26
     caractere_temp = ''
     
     resposta = [] 
