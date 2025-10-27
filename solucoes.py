305917e1a184da38370a2ac00c275d3469989310
Renata Meyer Hobold <renata.hobold@usp.br>
2025-10-26 21:22:19 -0300

    feat: implementa logica da funcao da cifra de cesar

--- solucoes.py
+++ solucoes.py
@@ -3,8 +3,23 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    # TODO: Implementar a lógica
-    pass
+    deslocamento = deslocamento%26
+    caractere_temp = ''
+    
+    resposta = [] 
+    # vou dar um join no final porque é mais rápido que concatenar string
+    
+    for caractere in texto:
+        caractere_temp = ord(caractere)
+    
+        if ('a' <= caractere) and (caractere <= 'z'):
+            caractere_temp = (caractere_temp+deslocamento-97)%26 + 97
+        elif ('A' <= caractere) and (caractere <= 'Z'):
+            caractere_temp = (caractere_temp+deslocamento-65)%26 + 65
+
+        resposta.append(chr(caractere_temp))
+        
+    return ''.join(resposta)
 
 def valida_cpf(cpf_string):
     # TODO: Implementar a lógica
