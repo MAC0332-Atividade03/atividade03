c197278dce8d5583b7025cf4c2daaecbf8f5a020
Fernando Drekner de Souza <fernandodrekner@usp.br>
2025-10-26 22:17:45 -0300

    feat: corrige a assinatura da função da maior palavra e a implementa

--- solucoes.py
+++ solucoes.py
@@ -6,6 +6,26 @@ def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar a lógica
     pass
 
-def valida_cpf(cpf_string):
-    # TODO: Implementar a lógica
-    pass
+def encontrar_maior_palavra(frase):
+    if isinstance(frase, str):
+        res = ""
+        maxLength = curStart = curLenght = 0
+
+        for i in range(len(frase)):
+            if(frase[i].isalnum()):
+                curLenght += 1
+            else:
+                if(frase[i] == ' ' and curLenght > maxLength):
+                    maxLength = curLenght
+                    res = frase[curStart:curStart+maxLength]
+                
+                curStart = i+1
+                curLenght = 0
+
+        if(curLenght > maxLength):
+            maxLength = curLenght
+            res = frase[curStart:curStart+maxLength]
+        
+        return res
+
+    return "ERRO: O parâmetro não é string"
