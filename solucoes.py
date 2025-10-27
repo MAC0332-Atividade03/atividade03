b3bd708bf971c672af95c9f0a8e0da6b22879a0a
senshiacy <uchiyama@usp.br>
2025-10-26 21:13:10 -0300

    feat: implementa lógica da função anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,41 @@
-def sao_anagramas(string1, string2):
-    # TODO: Implementar a lógica
-    pass
+def sao_anagramas(string1 : str, string2 : str) -> bool:
+    # Passar todos os maiúsculos para minúsculos
+    string1 = string1.lower().strip()
+    string2 = string2.lower().strip()
+
+    string1 = string1.split()
+    string2 = string2.split()
+
+    string1 = "".join(s.strip() for s in string1)
+    string2 = "".join(s.strip() for s in string2)
+
+    # Definir variáveis auxiliares
+    size1 = len(string1)
+    size2 = len(string2)
+    mapeador = {}
+
+    # Observar na tabela ASCII que os caracteres válidos estão entre 33 e 126, inclusos
+    for i in range(size1):
+        char_ascii = ord(string1[i])
+        if char_ascii not in mapeador:
+            mapeador[char_ascii] = 1
+        else:
+            mapeador[char_ascii] += 1
+    
+    # Para todo caractere lido, reduz um da contagem
+    for i in range(size2):
+        char_ascii = ord(string2[i])
+        if char_ascii not in mapeador:
+            mapeador[char_ascii] = -1
+        else:
+            mapeador[char_ascii] -= 1
+    
+    # Dos caracteres válidos, verifica se o mapeamento é nulo
+    for value in mapeador.values():
+        if(value != 0):
+            return False
+    
+    return True
 
 def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar a lógica
